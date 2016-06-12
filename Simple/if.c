539b2b5c8596917b0c9aa7ca06da0e8d3eb3cf4e
ponny35 <patipolsaechan@gmail.com>
2016-06-12 21:32:30 +0700

    Update if.c

---
+++ Simple/if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+main ( )
+{
+    int x;
+    scanf("%d",&x);
+    if(x%2==0)
+        printf("Even");
+    else
+        printf("Odd");
+    system("pause");
+}
